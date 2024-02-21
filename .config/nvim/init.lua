bc0963fae5c49a8929315ac04574848ed8d3ec97
Igor Michalak <git@igormichalak.com>
2024-02-21 17:53:25 +0100

    neovim: specify all plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,21 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.o.encoding = 'utf-8'
-vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 5
 vim.o.hlsearch = false
-
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.termguicolors = true
+vim.o.completeopt = 'menuone,noselect'
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
@@ -13,18 +25,32 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
-Plug 'tpope/vim-fugitive'
-
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
-Plug('nvim-treesitter/nvim-treesitter', { ["do"] = ':TSUpdate' })
+Plug 'nvim-telescope/telescope-file-browser.nvim'
 
+Plug('nvim-treesitter/nvim-treesitter', { ["do"] = ':TSUpdate' })
 Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+
+Plug 'windwp/nvim-autopairs'
+Plug 'numToStr/Comment.nvim'
+
+Plug 'tpope/vim-sleuth'
+Plug 'lukas-reineke/indent-blankline.nvim'
+
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-rhubarb'
+Plug 'lewis6991/gitsigns.nvim'
+
+Plug 'shaunsingh/solarized.nvim'
+Plug 'nvim-lualine/lualine.nvim'
 
 vim.call('plug#end')
 
-local lspconfig = require'lspconfig'
-
-lspconfig['lua_ls'].setup{}
-lspconfig['clangd'].setup{}
-
+require('solarized').set()
